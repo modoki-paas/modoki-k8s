ae05f4113df6d269244b5598203247a6d93535c1
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-14 19:47:24 +0900

    Remove auth_method column from users

--- schema/users.sql
+++ schema/users.sql
@@ -2,7 +2,6 @@ CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY,
     type ENUM("user", "group"),
     name VARCHAR(128) NOT NULL UNIQUE,
-    auth_method VARCHAR(64) NOT NULL DEFAULT "password",
     password VARCHAR(512),
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
