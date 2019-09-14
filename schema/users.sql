2af33ff83080fa6f9c700368edcf9f2d60e2f9c4
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-14 19:54:48 +0900

    Change user password length to 128

--- schema/users.sql
+++ schema/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY,
     type ENUM("user", "group"),
     name VARCHAR(128) NOT NULL UNIQUE,
-    password VARBINARY(512),
+    password VARBINARY(128),
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
