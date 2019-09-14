5404e611d7d96edad03a1963c3e585f92bc561ec
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-14 15:26:30 +0900

    Update users table schema

--- schema/users.sql
+++ schema/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(128) NOT NULL UNIQUE,
-    auth_method VARCHAR(64) NOT NULL DEFAULT "normal",
+    auth_method VARCHAR(64) NOT NULL DEFAULT "password",
     password VARCHAR(512),
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
