caa09887dbf6476bdcce5c000155ca2df147cdf5
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-16 01:11:20 +0900

    Update token struct for db

--- daemon/store/tokens.go
+++ daemon/store/tokens.go
@@ -2,13 +2,17 @@ package store
 
 import "time"
 
+type TokenPermission struct {
+}
+
 type Token struct {
-	ID        int       `db:"id"`
-	UserType  string    `db:"type"`
-	Name      string    `db:"name"`
-	Password  []byte    `db:"password"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID              int              `db:"id"`
+	Token           string           `db:"token"`
+	Organization    int              `db:"organization"`
+	Author          int              `db:"author"`
+	TokenPermission *TokenPermission `db:"permission"`
+	CreatedAt       time.Time        `db:"created_at"`
+	UpdatedAt       time.Time        `db:"updated_at"`
 }
 
 type tokensStore struct {
