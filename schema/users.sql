18878107ccbd7babe81f8c68651729c1dd74f7ad
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-15 12:51:31 +0900

    Change password db type to varchar

--- schema/users.sql
+++ schema/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY,
     type ENUM("user", "group"),
     name VARCHAR(128) NOT NULL UNIQUE,
-    password VARBINARY(128),
+    password VARCHAR(128),
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
