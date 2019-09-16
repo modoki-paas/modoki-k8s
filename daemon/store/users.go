942a3707d59a4e5012fe9850f77fc5870ac7dc48
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-17 01:17:36 +0900

    Add GetUserFromToken

--- daemon/store/users.go
+++ daemon/store/users.go
@@ -16,8 +16,8 @@ type User struct {
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
-// UserToken represents target user or organization(NOT AUTHOR OF TOKEN) and token
-type UserToken struct {
+// userToken represents target user or organization(NOT AUTHOR OF TOKEN) and token
+type userToken struct {
 	ID        int       `db:"id"`
 	UserType  string    `db:"type"`
 	Name      string    `db:"name"`
@@ -29,6 +29,8 @@ type UserToken struct {
 	Organization    int              `db:"organization"`
 	Author          int              `db:"author"`
 	TokenPermission *TokenPermission `db:"permission"`
+	TokenCreatedAt  time.Time        `db:"token_created_at"`
+	TokenUpdatedAt  time.Time        `db:"token_updated_at"`
 }
 
 type userStore struct {
@@ -39,17 +41,34 @@ func newUserStore(db *dbContext) *userStore {
 	return &userStore{db: db}
 }
 
-func (s *userStore) GetUserFromToken(token string) (*UserToken, error) {
-	var userToken UserToken
+func (s *userStore) GetUserFromToken(token string) (*User, *Token, error) {
+	var ut userToken
 	err := s.db.db.QueryRowxContext(
 		context.Background(),
-		"SELECT users.id, users.type, users.name, users.password, users.created_at, users.updated_at, tokens.token, tokens.organization, tokens.author, tokens.permission FROM users INNER JOIN tokens ON tokens.organization = user.id WHERE tokens.token = ?",
+		"SELECT users.id, users.type, users.name, users.password, users.created_at, users.updated_at, tokens.token, tokens.organization, tokens.author, tokens.permission tokens.created_at AS token_created_at, tokens.updated_at AS token_updated_at FROM users INNER JOIN tokens ON tokens.organization = user.id WHERE tokens.token = ?",
 		token,
-	).StructScan(&userToken)
+	).StructScan(&ut)
 
 	if err != nil {
-		return nil, xerrors.Errorf("failed to get token and user from db: %v", err)
+		return nil, nil, xerrors.Errorf("failed to get token and user from db: %v", err)
 	}
 
-	return &userToken, nil
+	u := &User{
+		ID:        ut.ID,
+		UserType:  ut.UserType,
+		Name:      ut.Name,
+		Password:  ut.Password,
+		CreatedAt: ut.CreatedAt,
+		UpdatedAt: ut.UpdatedAt,
+	}
+
+	t := &Token{
+		Token:        ut.Token,
+		Organization: ut.Organization,
+		Author:       ut.Author,
+		CreatedAt:    ut.TokenCreatedAt,
+		UpdatedAt:    ut.TokenUpdatedAt,
+	}
+
+	return u, t, nil
 }
