91bfa25b6df21e9aaf8f48dfa420b9b149a17c04
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-22 02:23:08 +0900

    Fix token test

--- daemon/store/tokens.go
+++ daemon/store/tokens.go
@@ -2,12 +2,30 @@ package store
 
 import (
 	"context"
+	"database/sql/driver"
+	"encoding/json"
 	"time"
 
 	"golang.org/x/xerrors"
 )
 
 type TokenPermission struct {
+}
+
+func (j *TokenPermission) Scan(src interface{}) error {
+	switch v := src.(type) {
+	case []byte:
+		if err := json.Unmarshal(v, j); err != nil {
+			return err
+		}
+	default:
+		return xerrors.Errorf("failed to scan json for %v", v)
+	}
+	return nil
+}
+
+func (j TokenPermission) Value() (driver.Value, error) {
+	return json.Marshal(j)
 }
 
 type Token struct {
@@ -26,6 +44,31 @@ type tokensStore struct {
 
 func newTokensStore(db *dbContext) *tokensStore {
 	return &tokensStore{db: db}
+}
+
+func (s *tokensStore) NewToken(t *Token) (*Token, error) {
+	res, err := s.db.db.ExecContext(
+		context.Background(),
+		"INSERT INTO tokens (token, organization, author, permission) VALUES (?, ?, ?, ?)",
+		t.Token,
+		t.Organization,
+		t.Author,
+		t.TokenPermission,
+	)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to register new token: %v", err)
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to register new token: %v", err)
+	}
+
+	t.ID = int(id)
+
+	return t, nil
 }
 
 func (s *tokensStore) GetFromToken(token string) (*Token, error) {
--- daemon/store/tokens_test.go
+++ daemon/store/tokens_test.go
@@ -4,16 +4,53 @@ package store
 
 import (
 	"testing"
+	"time"
+
+	"github.com/modoki-paas/modoki-k8s/daemon/testutil"
 )
 
 func TestGetFromToken(t *testing.T) {
-	// t.Run("success", func(t *testing.T) {
-	// 	db, mock := testutil.NewSQLMock(t)
+	t.Run("success", func(t *testing.T) {
+		db := testutil.NewSQLConn(t)
 
-	// 	mock.ExpectQuery("SELECT ")
+		store := NewDB(db)
 
-	// 	store := NewDB(db)
+		tk := &Token{
+			Token:           "token",
+			Organization:    1,
+			Author:          10,
+			TokenPermission: nil,
+		}
 
-	// 	store.Token().GetFromToken("my-token")
-	// })
+		_, err := store.Token().NewToken(tk)
+
+		if err != nil {
+			t.Errorf("failed to register new token: %v", err)
+		}
+
+		ret, err := store.Token().GetFromToken("token")
+
+		if err != nil {
+			t.Errorf("failed to retrieve token: %v", err)
+		}
+
+		if tk.ID != ret.ID {
+			t.Error("invalid id: want %v got %v", tk.ID, ret.ID)
+		}
+		if tk.Token != ret.Token {
+			t.Error("invalid token: want %v got %v", tk.Token, ret.Token)
+		}
+		if tk.Organization != ret.Organization {
+			t.Error("invalid org: want %v got %v", tk.Organization, ret.Organization)
+		}
+		if tk.Author != ret.Author {
+			t.Error("invalid author: want %v got %v", tk.Author, ret.Author)
+		}
+		if ret.CreatedAt == (time.Time{}) {
+			t.Error("invalid created_at", tk.CreatedAt)
+		}
+		if ret.UpdatedAt == (time.Time{}) {
+			t.Error("invalid updated_at", tk.UpdatedAt)
+		}
+	})
 }
