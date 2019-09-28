178baf368e550f7f8949f80ebac43bbf30554299
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-09-28 23:15:02 +0900

    Add AddService/GetService

--- daemon/store/services.go
+++ daemon/store/services.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"time"
 
+	"context"
+
 	"golang.org/x/xerrors"
 )
 
@@ -46,4 +48,56 @@ type serviceStore struct {
 
 func newServiceStore(db *dbContext) *serviceStore {
 	return &serviceStore{db: db}
+}
+
+func (ss *serviceStore) AddService(s *Service) (ret *Service, err error) {
+	dbx, err := ss.db.Begin(context.Background(), nil)
+	store := newDB(dbx)
+
+	if err != nil {
+		return nil, xerrors.Errorf("faield to begin transaction: %v", err)
+	}
+	defer func() {
+		if err != nil {
+			dbx.Rollback()
+		} else {
+			if err := dbx.Commit(); err != nil {
+				err = xerrors.Errorf("failed to commit transaction: %v", err)
+				ret = nil
+			}
+		}
+	}()
+
+	res, err := dbx.db.ExecContext(
+		context.Background(),
+		`INSERT INTO services
+			(owner, name, config)
+			VALUES (?, ?, ?)`,
+		s.Owner, s.Name, s.Config,
+	)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to add service to db: %v", err)
+	}
+
+	id64, err := res.LastInsertId()
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to add service to db: %v", err)
+	}
+
+	return store.Service().GetService(int(id64))
+}
+
+func (ss *serviceStore) GetService(id int) (*Service, error) {
+	var service Service
+	err := ss.db.db.
+		QueryRowxContext(context.Background(), "SELECT * FROM services WHERE id=?", id).
+		StructScan(&service)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to retrieve service: %v", err)
+	}
+
+	return &service, nil
 }
