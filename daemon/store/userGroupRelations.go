af34e680971a4ec0754d26eaee7b81e90564f918
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-10-01 23:56:53 +0900

    Update UserGroupRelation struct

--- daemon/store/userGroupRelations.go
+++ daemon/store/userGroupRelations.go
@@ -1,14 +1,36 @@
 package store
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+
+	"golang.org/x/xerrors"
+)
+
+type UserPermission struct {
+}
+
+func (j *UserPermission) Scan(src interface{}) error {
+	switch v := src.(type) {
+	case []byte:
+		if err := json.Unmarshal(v, j); err != nil {
+			return err
+		}
+	default:
+		return xerrors.Errorf("failed to scan json for %v", v)
+	}
+	return nil
+}
+
+func (j UserPermission) Value() (driver.Value, error) {
+	return json.Marshal(j)
+}
 
 type UserGroupRelation struct {
-	ID        int       `db:"id"`
-	UserType  string    `db:"type"`
-	Name      string    `db:"name"`
-	Password  []byte    `db:"password"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID         int             `db:"id"`
+	GroupID    int             `db:"group_id"`
+	UserID     int             `db:"user_id"`
+	Permission *UserPermission `db:"permission"`
 }
 
 type userGroupRelationsStore struct {
@@ -18,4 +40,3 @@ type userGroupRelationsStore struct {
 func newUserGroupRelationsStore(db *dbContext) *userGroupRelationsStore {
 	return &userGroupRelationsStore{db: db}
 }
-
