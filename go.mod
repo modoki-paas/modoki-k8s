d28cb634f02674b07746262908fbd2c106ea5e40
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-12-02 23:34:07 +0900

    Update pkg/kustomizer

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/golang/protobuf v1.3.2
 	github.com/google/go-cmp v0.3.1
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/otiai10/copy v1.0.2
 	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392
 	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
--- pkg/kustomizer/kustomizer.go
+++ pkg/kustomizer/kustomizer.go
@@ -1,2 +1,53 @@
 package main
 
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/docker/docker/pkg/testutil/cmd"
+	"github.com/otiai10/copy"
+
+	"golang.org/x/xerrors"
+)
+
+var (
+	// OriginalDir is the path to the original yaml folder
+	OriginalDir = "./templates"
+)
+
+type Workspace struct {
+	Dir string
+}
+
+func NewWorkspace() (*Workspace, error) {
+	d, err := ioutil.TempDir("", "yamler-")
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to create workspace dir: %w", err)
+	}
+
+	copy.Copy(OriginalDir, d)
+
+	return &Workspace{Dir: d}, nil
+}
+
+func (w *Workspace) Close() error {
+	return os.RemoveAll(w.Dir)
+}
+
+func (w *Workspace) CommandWithInput(ctx context.Context, input io.Reader, command string, args ...string) *cmd.Cmd {
+	cmd := exec.CommandContext(ctx, command, args...)
+
+	cmd.Dir = w.Dir
+	cmd.Env = os.Environ()
+
+	return cmd
+}
+
+func (w *Workspace) Command(ctx context.Context, command string, args ...string) *exec.Cmd {
+	return w.CommandWithInput(ctx, bytes.NewReader(nil), command, args...)
+}
