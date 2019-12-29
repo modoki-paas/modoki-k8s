3699e053e6ec55fe38980d78d07e4adc529da75a
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2019-12-29 20:30:52 +0900

    Add transaction util

--- apiserver/handler/userOrg.go
+++ apiserver/handler/userOrg.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/jmoiron/sqlx"
 	modoki "github.com/modoki-paas/modoki-k8s/api"
 	"github.com/modoki-paas/modoki-k8s/apiserver/store/users"
+	"github.com/modoki-paas/modoki-k8s/internal/dbutil"
 	"github.com/modoki-paas/modoki-k8s/pkg/auth"
 	"github.com/modoki-paas/modoki-k8s/pkg/rbac/permissions"
 	"github.com/modoki-paas/modoki-k8s/pkg/rbac/roles"
@@ -20,33 +22,42 @@ type UserOrgServer struct {
 
 var _ modoki.UserOrgServer = &UserOrgServer{}
 
-func (s *UserOrgServer) UserAdd(ctx context.Context, req *modoki.UserAddRequest) (*modoki.UserAddResponse, error) {
+func (s *UserOrgServer) UserAdd(ctx context.Context, req *modoki.UserAddRequest) (res *modoki.UserAddResponse, err error) {
 	if err := auth.IsAuthorized(ctx, permissions.UserCreate); err != nil {
 		return nil, status.Error(codes.PermissionDenied, err.Error())
 	}
 
-	userStore := users.NewUserStore(s.Context.DB)
+	err = dbutil.Transaction(ctx, s.Context.DB, func(tx *sqlx.Tx) error {
+		userStore := users.NewUserStore(s.Context.DB)
 
-	user := req.User
+		user := req.User
 
-	systemRole := roles.FindSystemRole(user.RoleName)
+		systemRole := roles.FindSystemRole(user.RoleName)
 
-	if systemRole == nil {
-		return nil, status.Error(codes.InvalidArgument, "unknown system role")
-	}
-
-	_, err := userStore.AddUser(user.UserId, user.Name, types.UserNormal, systemRole.Name)
-	if err != nil {
-		if err == users.ErrUserIDDuplicates {
-			return nil, status.Error(codes.AlreadyExists, "user id already exists")
+		if systemRole == nil {
+			return status.Error(codes.InvalidArgument, "unknown system role")
 		}
 
-		return nil, status.Error(codes.Internal, fmt.Sprintf("internal error: %s", err.Error()))
+		_, err := userStore.AddUser(user.UserId, user.Name, types.UserNormal, systemRole.Name)
+		if err != nil {
+			if err == users.ErrUserIDDuplicates {
+				return status.Error(codes.AlreadyExists, "user id already exists")
+			}
+
+			return status.Error(codes.Internal, fmt.Sprintf("internal error: %s", err.Error()))
+		}
+
+		res = &modoki.UserAddResponse{
+			User: user,
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
 	}
 
-	return &modoki.UserAddResponse{
-		User: user,
-	}, nil
+	return res, nil
 }
 
 func (s *UserOrgServer) UserDelete(_ context.Context, _ *modoki.UserDeleteRequest) (*modoki.UserDeleteResponse, error) {
---
+++ internal/dbutil/tx.go
@@ -0,0 +1,39 @@
+package dbutil
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/xerrors"
+)
+
+func Transaction(ctx context.Context, db *sqlx.DB, fn func(tx *sqlx.Tx) error) (err error) {
+	tx, err := db.BeginTxx(ctx, nil)
+
+	if err != nil {
+		return xerrors.Errorf("failed to begin transaction: %w", err)
+	}
+
+	defer func() {
+		if e := recover(); e != nil {
+			if e, ok := e.(error); ok {
+				err = xerrors.Errorf("recovered from panic: %w", e)
+			} else {
+				err = xerrors.Errorf("recovered from panic: %v", e)
+			}
+		}
+	}()
+
+	if err := fn(tx); err != nil {
+		tx.Rollback()
+
+		// Don't wrap for status.Error()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return xerrors.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return nil
+}
