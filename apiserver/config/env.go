2d85edc7072e0747b879731ea88504b967f4dfbf
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-01-01 18:45:09 +0900

    Fix mysql url

--- apiserver/config/env.go
+++ apiserver/config/env.go
@@ -19,7 +19,7 @@ func ReadEnv() *EnvConfig {
 
 	if db == "" {
 		db = fmt.Sprintf(
-			"mysql://%s:%s@tcp(%s:%s)/%s?parseTime=true",
+			"%s:%s@tcp(%s:%s)/%s?parseTime=true",
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_HOST"),
