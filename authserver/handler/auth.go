26042c343676a53a24bf47dec99d1be4eed75950
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-01-08 13:03:23 +0900

    Add auth service

---
+++ authserver/handler/auth.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"context"
+
+	modoki "github.com/modoki-paas/modoki-k8s/api"
+)
+
+type AuthServer struct {
+}
+
+func (s *AuthServer) SignIn(ctx context.Context, in *modoki.SignInRequest) (*modoki.SignInResponse, error) {
+	panic("not implemented")
+}
+
+func (s *AuthServer) SignOut(_ context.Context, _ *modoki.SignOutRequest) (*modoki.SignOutResponse, error) {
+	panic("not implemented")
+}
+
+func (s *AuthServer) Callback(_ context.Context, _ *modoki.CallbackRequest) (*modoki.CallbackResponse, error) {
+	panic("not implemented")
+}
