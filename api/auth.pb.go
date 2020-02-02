28f37da30f527c0a466c64ff9e42d5c26933ed65
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-02 20:52:34 +0900

    Add roles for issueing unbound tokens and binding tokens

--- api/auth.pb.go
+++ api/auth.pb.go
@@ -3,13 +3,14 @@
 
 package modoki
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
 import (
-	context "golang.org/x/net/context"
+	context "context"
+	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -17,22 +18,74 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+
 type SignInRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *SignInRequest) Reset()                    { *m = SignInRequest{} }
-func (m *SignInRequest) String() string            { return proto.CompactTextString(m) }
-func (*SignInRequest) ProtoMessage()               {}
-func (*SignInRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{0} }
+func (m *SignInRequest) Reset()         { *m = SignInRequest{} }
+func (m *SignInRequest) String() string { return proto.CompactTextString(m) }
+func (*SignInRequest) ProtoMessage()    {}
+func (*SignInRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{0}
+}
+
+func (m *SignInRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignInRequest.Unmarshal(m, b)
+}
+func (m *SignInRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignInRequest.Marshal(b, m, deterministic)
+}
+func (m *SignInRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignInRequest.Merge(m, src)
+}
+func (m *SignInRequest) XXX_Size() int {
+	return xxx_messageInfo_SignInRequest.Size(m)
+}
+func (m *SignInRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignInRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignInRequest proto.InternalMessageInfo
 
 type SignInResponse struct {
-	RedirectUri string `protobuf:"bytes,1,opt,name=redirect_uri,json=redirectUri" json:"redirect_uri,omitempty"`
+	RedirectUri          string   `protobuf:"bytes,1,opt,name=redirect_uri,json=redirectUri,proto3" json:"redirect_uri,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *SignInResponse) Reset()                    { *m = SignInResponse{} }
-func (m *SignInResponse) String() string            { return proto.CompactTextString(m) }
-func (*SignInResponse) ProtoMessage()               {}
-func (*SignInResponse) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{1} }
+func (m *SignInResponse) Reset()         { *m = SignInResponse{} }
+func (m *SignInResponse) String() string { return proto.CompactTextString(m) }
+func (*SignInResponse) ProtoMessage()    {}
+func (*SignInResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{1}
+}
+
+func (m *SignInResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignInResponse.Unmarshal(m, b)
+}
+func (m *SignInResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignInResponse.Marshal(b, m, deterministic)
+}
+func (m *SignInResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignInResponse.Merge(m, src)
+}
+func (m *SignInResponse) XXX_Size() int {
+	return xxx_messageInfo_SignInResponse.Size(m)
+}
+func (m *SignInResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignInResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignInResponse proto.InternalMessageInfo
 
 func (m *SignInResponse) GetRedirectUri() string {
 	if m != nil {
@@ -42,31 +95,100 @@ func (m *SignInResponse) GetRedirectUri() string {
 }
 
 type SignOutRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *SignOutRequest) Reset()                    { *m = SignOutRequest{} }
-func (m *SignOutRequest) String() string            { return proto.CompactTextString(m) }
-func (*SignOutRequest) ProtoMessage()               {}
-func (*SignOutRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{2} }
+func (m *SignOutRequest) Reset()         { *m = SignOutRequest{} }
+func (m *SignOutRequest) String() string { return proto.CompactTextString(m) }
+func (*SignOutRequest) ProtoMessage()    {}
+func (*SignOutRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{2}
+}
+
+func (m *SignOutRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignOutRequest.Unmarshal(m, b)
+}
+func (m *SignOutRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignOutRequest.Marshal(b, m, deterministic)
+}
+func (m *SignOutRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignOutRequest.Merge(m, src)
+}
+func (m *SignOutRequest) XXX_Size() int {
+	return xxx_messageInfo_SignOutRequest.Size(m)
+}
+func (m *SignOutRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignOutRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignOutRequest proto.InternalMessageInfo
 
 type SignOutResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *SignOutResponse) Reset()                    { *m = SignOutResponse{} }
-func (m *SignOutResponse) String() string            { return proto.CompactTextString(m) }
-func (*SignOutResponse) ProtoMessage()               {}
-func (*SignOutResponse) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{3} }
+func (m *SignOutResponse) Reset()         { *m = SignOutResponse{} }
+func (m *SignOutResponse) String() string { return proto.CompactTextString(m) }
+func (*SignOutResponse) ProtoMessage()    {}
+func (*SignOutResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{3}
+}
+
+func (m *SignOutResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignOutResponse.Unmarshal(m, b)
+}
+func (m *SignOutResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignOutResponse.Marshal(b, m, deterministic)
+}
+func (m *SignOutResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignOutResponse.Merge(m, src)
+}
+func (m *SignOutResponse) XXX_Size() int {
+	return xxx_messageInfo_SignOutResponse.Size(m)
+}
+func (m *SignOutResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignOutResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignOutResponse proto.InternalMessageInfo
 
 type CallbackRequest struct {
-	AccessToken string `protobuf:"bytes,1,opt,name=access_token,json=accessToken" json:"access_token,omitempty"`
-	IdToken     string `protobuf:"bytes,2,opt,name=id_token,json=idToken" json:"id_token,omitempty"`
-	State       string `protobuf:"bytes,3,opt,name=state" json:"state,omitempty"`
+	AccessToken          string   `protobuf:"bytes,1,opt,name=access_token,json=accessToken,proto3" json:"access_token,omitempty"`
+	IdToken              string   `protobuf:"bytes,2,opt,name=id_token,json=idToken,proto3" json:"id_token,omitempty"`
+	State                string   `protobuf:"bytes,3,opt,name=state,proto3" json:"state,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CallbackRequest) Reset()                    { *m = CallbackRequest{} }
-func (m *CallbackRequest) String() string            { return proto.CompactTextString(m) }
-func (*CallbackRequest) ProtoMessage()               {}
-func (*CallbackRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{4} }
+func (m *CallbackRequest) Reset()         { *m = CallbackRequest{} }
+func (m *CallbackRequest) String() string { return proto.CompactTextString(m) }
+func (*CallbackRequest) ProtoMessage()    {}
+func (*CallbackRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{4}
+}
+
+func (m *CallbackRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CallbackRequest.Unmarshal(m, b)
+}
+func (m *CallbackRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CallbackRequest.Marshal(b, m, deterministic)
+}
+func (m *CallbackRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CallbackRequest.Merge(m, src)
+}
+func (m *CallbackRequest) XXX_Size() int {
+	return xxx_messageInfo_CallbackRequest.Size(m)
+}
+func (m *CallbackRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CallbackRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CallbackRequest proto.InternalMessageInfo
 
 func (m *CallbackRequest) GetAccessToken() string {
 	if m != nil {
@@ -90,12 +212,35 @@ func (m *CallbackRequest) GetState() string {
 }
 
 type CallbackResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CallbackResponse) Reset()                    { *m = CallbackResponse{} }
-func (m *CallbackResponse) String() string            { return proto.CompactTextString(m) }
-func (*CallbackResponse) ProtoMessage()               {}
-func (*CallbackResponse) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{5} }
+func (m *CallbackResponse) Reset()         { *m = CallbackResponse{} }
+func (m *CallbackResponse) String() string { return proto.CompactTextString(m) }
+func (*CallbackResponse) ProtoMessage()    {}
+func (*CallbackResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8bbd6f3875b0e874, []int{5}
+}
+
+func (m *CallbackResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CallbackResponse.Unmarshal(m, b)
+}
+func (m *CallbackResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CallbackResponse.Marshal(b, m, deterministic)
+}
+func (m *CallbackResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CallbackResponse.Merge(m, src)
+}
+func (m *CallbackResponse) XXX_Size() int {
+	return xxx_messageInfo_CallbackResponse.Size(m)
+}
+func (m *CallbackResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_CallbackResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CallbackResponse proto.InternalMessageInfo
 
 func init() {
 	proto.RegisterType((*SignInRequest)(nil), "modoki.SignInRequest")
@@ -106,6 +251,29 @@ func init() {
 	proto.RegisterType((*CallbackResponse)(nil), "modoki.CallbackResponse")
 }
 
+func init() { proto.RegisterFile("auth.proto", fileDescriptor_8bbd6f3875b0e874) }
+
+var fileDescriptor_8bbd6f3875b0e874 = []byte{
+	// 262 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x91, 0xbb, 0x4f, 0xc3, 0x30,
+	0x10, 0xc6, 0x55, 0x1e, 0x69, 0x38, 0x1e, 0x29, 0x16, 0x8f, 0x90, 0x09, 0x3c, 0x31, 0x65, 0xa0,
+	0x03, 0x12, 0x12, 0x03, 0x30, 0x31, 0x21, 0x15, 0x58, 0x58, 0x2a, 0x37, 0x39, 0x51, 0x2b, 0x25,
+	0x2e, 0xf6, 0xf9, 0x9f, 0xe3, 0xaf, 0x43, 0xcd, 0xd9, 0xad, 0xda, 0x8e, 0xfe, 0x7d, 0x77, 0xdf,
+	0xf9, 0xbb, 0x03, 0x50, 0x9e, 0xa6, 0xe5, 0xdc, 0x1a, 0x32, 0x22, 0xf9, 0x31, 0xb5, 0x69, 0xb4,
+	0xcc, 0xe0, 0xf8, 0x5d, 0x7f, 0xb7, 0xaf, 0xed, 0x08, 0x7f, 0x3d, 0x3a, 0x92, 0x43, 0x38, 0x89,
+	0xc0, 0xcd, 0x4d, 0xeb, 0x50, 0xdc, 0xc0, 0x91, 0xc5, 0x5a, 0x5b, 0xac, 0x68, 0xec, 0xad, 0xce,
+	0x7b, 0xd7, 0xbd, 0xdb, 0x83, 0xd1, 0x61, 0x64, 0x9f, 0x56, 0xcb, 0x01, 0x37, 0xbd, 0x79, 0x8a,
+	0x36, 0xa7, 0x90, 0x2d, 0x09, 0xfb, 0x48, 0x84, 0xec, 0x45, 0xcd, 0x66, 0x13, 0x55, 0x35, 0xa1,
+	0x6a, 0x61, 0xad, 0xaa, 0x0a, 0x9d, 0x1b, 0x93, 0x69, 0xb0, 0x8d, 0xd6, 0xcc, 0x3e, 0x16, 0x48,
+	0x5c, 0x41, 0xaa, 0xeb, 0x20, 0xef, 0x74, 0x72, 0x5f, 0xd7, 0x2c, 0x9d, 0xc1, 0xbe, 0x23, 0x45,
+	0x98, 0xef, 0x76, 0x9c, 0x1f, 0x52, 0xc0, 0x60, 0x35, 0x86, 0x47, 0xdf, 0xfd, 0xf5, 0x60, 0xef,
+	0xc9, 0xd3, 0x54, 0xdc, 0x43, 0xc2, 0xe9, 0xc4, 0x79, 0xc9, 0x1b, 0x28, 0xd7, 0xe2, 0x17, 0x17,
+	0x9b, 0x38, 0x2c, 0xe1, 0x01, 0xfa, 0x21, 0x8f, 0x58, 0x2b, 0x59, 0x45, 0x2e, 0x2e, 0xb7, 0x78,
+	0xe8, 0x7d, 0x84, 0x34, 0xfe, 0x48, 0x2c, 0x8b, 0x36, 0x56, 0x51, 0xe4, 0xdb, 0x02, 0xb7, 0x3f,
+	0xa7, 0x5f, 0xe1, 0x58, 0x93, 0xa4, 0xbb, 0xdd, 0xf0, 0x3f, 0x00, 0x00, 0xff, 0xff, 0x8c, 0xd7,
+	0x27, 0xab, 0xc9, 0x01, 0x00, 0x00,
+}
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
 var _ grpc.ClientConn
@@ -114,8 +282,9 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for Auth service
-
+// AuthClient is the client API for Auth service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type AuthClient interface {
 	SignIn(ctx context.Context, in *SignInRequest, opts ...grpc.CallOption) (*SignInResponse, error)
 	SignOut(ctx context.Context, in *SignOutRequest, opts ...grpc.CallOption) (*SignOutResponse, error)
@@ -132,7 +301,7 @@ func NewAuthClient(cc *grpc.ClientConn) AuthClient {
 
 func (c *authClient) SignIn(ctx context.Context, in *SignInRequest, opts ...grpc.CallOption) (*SignInResponse, error) {
 	out := new(SignInResponse)
-	err := grpc.Invoke(ctx, "/modoki.Auth/SignIn", in, out, c.cc, opts...)
+	err := c.cc.Invoke(ctx, "/modoki.Auth/SignIn", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -141,7 +310,7 @@ func (c *authClient) SignIn(ctx context.Context, in *SignInRequest, opts ...grpc
 
 func (c *authClient) SignOut(ctx context.Context, in *SignOutRequest, opts ...grpc.CallOption) (*SignOutResponse, error) {
 	out := new(SignOutResponse)
-	err := grpc.Invoke(ctx, "/modoki.Auth/SignOut", in, out, c.cc, opts...)
+	err := c.cc.Invoke(ctx, "/modoki.Auth/SignOut", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -150,19 +319,32 @@ func (c *authClient) SignOut(ctx context.Context, in *SignOutRequest, opts ...gr
 
 func (c *authClient) Callback(ctx context.Context, in *CallbackRequest, opts ...grpc.CallOption) (*CallbackResponse, error) {
 	out := new(CallbackResponse)
-	err := grpc.Invoke(ctx, "/modoki.Auth/Callback", in, out, c.cc, opts...)
+	err := c.cc.Invoke(ctx, "/modoki.Auth/Callback", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// Server API for Auth service
-
+// AuthServer is the server API for Auth service.
 type AuthServer interface {
 	SignIn(context.Context, *SignInRequest) (*SignInResponse, error)
 	SignOut(context.Context, *SignOutRequest) (*SignOutResponse, error)
 	Callback(context.Context, *CallbackRequest) (*CallbackResponse, error)
+}
+
+// UnimplementedAuthServer can be embedded to have forward compatible implementations.
+type UnimplementedAuthServer struct {
+}
+
+func (*UnimplementedAuthServer) SignIn(ctx context.Context, req *SignInRequest) (*SignInResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SignIn not implemented")
+}
+func (*UnimplementedAuthServer) SignOut(ctx context.Context, req *SignOutRequest) (*SignOutResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SignOut not implemented")
+}
+func (*UnimplementedAuthServer) Callback(ctx context.Context, req *CallbackRequest) (*CallbackResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Callback not implemented")
 }
 
 func RegisterAuthServer(s *grpc.Server, srv AuthServer) {
@@ -242,27 +424,4 @@ var _Auth_serviceDesc = grpc.ServiceDesc{
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "auth.proto",
-}
-
-func init() { proto.RegisterFile("auth.proto", fileDescriptor1) }
-
-var fileDescriptor1 = []byte{
-	// 262 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x91, 0xbb, 0x4f, 0xc3, 0x30,
-	0x10, 0xc6, 0x55, 0x1e, 0x69, 0x38, 0x1e, 0x29, 0x16, 0x8f, 0x90, 0x09, 0x3c, 0x31, 0x65, 0xa0,
-	0x03, 0x12, 0x12, 0x03, 0x30, 0x31, 0x21, 0x15, 0x58, 0x58, 0x2a, 0x37, 0x39, 0x51, 0x2b, 0x25,
-	0x2e, 0xf6, 0xf9, 0x9f, 0xe3, 0xaf, 0x43, 0xcd, 0xd9, 0xad, 0xda, 0x8e, 0xfe, 0x7d, 0x77, 0xdf,
-	0xf9, 0xbb, 0x03, 0x50, 0x9e, 0xa6, 0xe5, 0xdc, 0x1a, 0x32, 0x22, 0xf9, 0x31, 0xb5, 0x69, 0xb4,
-	0xcc, 0xe0, 0xf8, 0x5d, 0x7f, 0xb7, 0xaf, 0xed, 0x08, 0x7f, 0x3d, 0x3a, 0x92, 0x43, 0x38, 0x89,
-	0xc0, 0xcd, 0x4d, 0xeb, 0x50, 0xdc, 0xc0, 0x91, 0xc5, 0x5a, 0x5b, 0xac, 0x68, 0xec, 0xad, 0xce,
-	0x7b, 0xd7, 0xbd, 0xdb, 0x83, 0xd1, 0x61, 0x64, 0x9f, 0x56, 0xcb, 0x01, 0x37, 0xbd, 0x79, 0x8a,
-	0x36, 0xa7, 0x90, 0x2d, 0x09, 0xfb, 0x48, 0x84, 0xec, 0x45, 0xcd, 0x66, 0x13, 0x55, 0x35, 0xa1,
-	0x6a, 0x61, 0xad, 0xaa, 0x0a, 0x9d, 0x1b, 0x93, 0x69, 0xb0, 0x8d, 0xd6, 0xcc, 0x3e, 0x16, 0x48,
-	0x5c, 0x41, 0xaa, 0xeb, 0x20, 0xef, 0x74, 0x72, 0x5f, 0xd7, 0x2c, 0x9d, 0xc1, 0xbe, 0x23, 0x45,
-	0x98, 0xef, 0x76, 0x9c, 0x1f, 0x52, 0xc0, 0x60, 0x35, 0x86, 0x47, 0xdf, 0xfd, 0xf5, 0x60, 0xef,
-	0xc9, 0xd3, 0x54, 0xdc, 0x43, 0xc2, 0xe9, 0xc4, 0x79, 0xc9, 0x1b, 0x28, 0xd7, 0xe2, 0x17, 0x17,
-	0x9b, 0x38, 0x2c, 0xe1, 0x01, 0xfa, 0x21, 0x8f, 0x58, 0x2b, 0x59, 0x45, 0x2e, 0x2e, 0xb7, 0x78,
-	0xe8, 0x7d, 0x84, 0x34, 0xfe, 0x48, 0x2c, 0x8b, 0x36, 0x56, 0x51, 0xe4, 0xdb, 0x02, 0xb7, 0x3f,
-	0xa7, 0x5f, 0xe1, 0x58, 0x93, 0xa4, 0xbb, 0xdd, 0xf0, 0x3f, 0x00, 0x00, 0xff, 0xff, 0x8c, 0xd7,
-	0x27, 0xab, 0xc9, 0x01, 0x00, 0x00,
 }
--- pkg/rbac/permissions/token.go
+++ pkg/rbac/permissions/token.go
@@ -1,9 +1,11 @@
 package permissions
 
 var (
-	TokenIssue    = NewPermission("token:v1alpha1:issue", NamespaceUser, NamespaceOrg)
-	TokenDelete   = NewPermission("token:v1alpha1:delete", NamespaceUser, NamespaceOrg)
-	TokenValidate = NewPermission("token:v1alpha:validate")
+	TokenIssue        = NewPermission("token:v1alpha1:issue", NamespaceUser, NamespaceOrg)
+	TokenIssueUnbound = NewPermission("token:v1alpha1:unbound_issue", NamespaceUser, NamespaceOrg) // For OpenID Connect
+	TokenBindTarget   = NewPermission("token:v1alpha1:bind_target", NamespaceUser, NamespaceOrg)   // For OpenID Connect
+	TokenDelete       = NewPermission("token:v1alpha1:delete", NamespaceUser, NamespaceOrg)
+	TokenValidate     = NewPermission("token:v1alpha:validate")
 
 	TokenPermissions = []*Permission{TokenIssue, TokenDelete}
 )
