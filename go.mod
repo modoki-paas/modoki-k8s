bcfd7f21652abbc9ecbdc75b5940fd7f44084f8c
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-03 23:21:43 +0900

    Add Login/Callback for OpenID Connect Authenticator

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/otiai10/copy v1.0.2
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/rs/xid v1.2.1
-	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392
+	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	golang.org/x/sys v0.0.0-20200106162015-b016eb3dc98e // indirect
--- internal/oidc/oidc.go
+++ internal/oidc/oidc.go
@@ -2,18 +2,29 @@ package oidc
 
 import (
 	"context"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
 
 	"github.com/coreos/go-oidc"
+	"github.com/modoki-paas/modoki-k8s/internal/tokenutil"
 	"golang.org/x/oauth2"
 	"golang.org/x/xerrors"
 )
 
+// Authenticator is a helper to sign in with OpenID Connect
 type Authenticator struct {
 	Provider *oidc.Provider
 	Config   oauth2.Config
-	Ctx      context.Context
 }
 
+// AuthResult represents results of OpenID Connect authentication flow
+type AuthResult struct {
+	Token   *oauth2.Token
+	IDToken *oidc.IDToken
+	Profile map[string]interface{}
+}
+
+// NewAuthenticator initializes an Authenticator
 func NewAuthenticator(ctx context.Context, clientID, clientSecret, redirectURL, providerURL string, scopes []string) (*Authenticator, error) {
 	provider, err := oidc.NewProvider(ctx, providerURL)
 	if err != nil {
@@ -32,4 +43,58 @@ func NewAuthenticator(ctx context.Context, clientID, clientSecret, redirectURL, 
 		Provider: provider,
 		Config:   conf,
 	}, nil
+}
+
+// Callback handles callback requests from IdP
+func (author *Authenticator) Callback(ctx context.Context, expectedState, actualState, code string) (*AuthResult, error) {
+	token, err := author.Config.Exchange(ctx, code)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to exchange code: %w", err)
+	}
+
+	rawIDToken, ok := token.Extra("id_token").(string)
+	if !ok {
+		return nil, xerrors.Errorf("No id_token field in oauth2 token: %w", http.StatusInternalServerError)
+	}
+
+	oidcConfig := &oidc.Config{
+		ClientID: author.Config.ClientID,
+	}
+
+	idToken, err := author.Provider.Verifier(oidcConfig).Verify(ctx, rawIDToken)
+
+	if err != nil {
+		return nil, xerrors.Errorf("Failed to verify ID Token: %w", err)
+	}
+
+	// Getting now the userInfo
+	var profile map[string]interface{}
+	if err := idToken.Claims(&profile); err != nil {
+		return nil, xerrors.Errorf("failed to get userinfo: %w", err)
+	}
+
+	return &AuthResult{
+		Token:   token,
+		IDToken: idToken,
+		Profile: profile,
+	}, nil
+}
+
+// Login generates state and redirect url to IdP
+func (author *Authenticator) Login(ctx context.Context, salt string) (redirectURL, state string, err error) {
+	state, err = tokenutil.GenerateRandomToken()
+
+	if err != nil {
+		return "", "", xerrors.Errorf("failed to generate a random token: %w", err)
+	}
+
+	hashedState, err := bcrypt.GenerateFromPassword([]byte(state), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", "", xerrors.Errorf("failed to hash the state string: %w", err)
+	}
+
+	redirectURL = author.Config.AuthCodeURL(string(hashedState))
+
+	return
 }
