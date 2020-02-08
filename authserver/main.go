2e4cd87650ab5ae7b2f659463db2e7ce02e3cc3a
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-08 20:21:31 +0900

    Register auth service to gRPC server

--- authserver/main.go
+++ authserver/main.go
@@ -78,6 +78,7 @@ func initGRPCServer(sctx *handler.ServerContext) (*grpc.Server, error) {
 	api.RegisterUserOrgServer(server, &handler.UserOrgServer{Context: sctx})
 	api.RegisterTokenServer(server, &handler.TokenServer{Context: sctx})
 	api.RegisterAppServer(server, &handler.AppServer{Context: sctx})
+	api.RegisterAuthServer(server, &handler.AuthServer{Context: sctx})
 
 	return server, nil
 }
